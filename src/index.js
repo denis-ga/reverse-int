f7fee347a82d2193464660976475ea1c8d2cc0c9
Denis G <denis.g@engagecraft.com>
2023-08-05 12:23:37 +0300

    feat: completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let number = n.toString().replace('-','');
+    let chars = number.split('');
+
+    if(chars.pop === '0')
+        chars.slice(0, -1);
+
+    return chars.reverse().join('');
 }
